4cde6851c8d94d3af224dcc3d7c1c370bfbe0267
realhansookim <kimhansu115@gmail.com>
2023-01-03 12:55:01 +0900

    첫번째커밋

---
+++ src/main/java/com/first_project/justdoeat/JustdoeatApplication.java
@@ -0,0 +1,13 @@
+package com.first_project.justdoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustdoeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustdoeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first_project/justdoeat/JustdoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_project.justdoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustdoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
